da7426ae3b295d3880eeefdba9c37b7ef44b4e39
hsh-newbi <hsonghart0426@gmail.com>
2021-03-20 02:18:47 +0900

    투두리스트 입력 시 저장
    로컬스토리지에 객체로 투두리스트 값 저장
    투두리스트 id 랜덤부여
    투두리스트 페인트 테스트

--- resource/js/toDoList.js
+++ resource/js/toDoList.js
@@ -8,19 +8,36 @@ function saveToDo(){
   localStorage.setItem(TODO_LS, JSON.stringify(todos));
 }
 
+function paintToDo(text){
+  const paintList = document.createElement("article"),
+    toDoContainer = document.querySelector(".todo-list");
+
+  toDoContainer.appendChild(paintList);
+  paintList.innerText = text;
+}
+
 function displayInputTodo(){
-  const input = document.createElement("input")
+  const input = document.createElement("input"),
+  newId = Math.random().toString(36).substr(2, 16);
+
   toDoInputBox.appendChild(input);
   input.classList.add("input-todo");
   input.placeholder = "✍🏻 내용을 입력하세요.";
+  input.id = newId;
 
-  const inputToDo = toDoInputBox.querySelector(".input-todo");
-  
   toDoInputBox.addEventListener("submit", (e)=>{ // toDoInput 이벤트 리스너
     e.preventDefault();
-      todos.push(input.value);
+    const toDoObj = {
+      id : newId,
+      text : input.value
+    }
+      todos.push(toDoObj);
       saveToDo();
+      // paint todolist
+      paintToDo(input.value)
+      // reset input value
       input.value = "";
+    
     }
   )
 }
