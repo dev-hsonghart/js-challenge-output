68507c7292bf5452fa97e6a441392db12f8b9f0f
hsh-newbi <hsonghart0426@gmail.com>
2021-03-21 01:43:51 +0900

    배경이미지 랜덤 변경
    나중에 복기 필요.
    이미지가 교차해서 애니메이션 되는것도 추가 디벨롭

---
+++ resource/js/controlBG.js
@@ -0,0 +1,23 @@
+const bg = document.querySelector(".background-container");
+  
+
+function getRandomNum(){
+  const number = Math.floor(Math.random() * 4);
+  return number;
+}
+
+function paintImg(num){
+  const image = new Image();
+  console.dir(image);
+  image.src = `images/img/0${num + 1}.jpg`;
+  bg.appendChild(image)
+  image.classList.add("background-img");
+  
+}
+
+function init(){
+  const randomNum = getRandomNum();
+  paintImg(randomNum);
+}
+
+init()
