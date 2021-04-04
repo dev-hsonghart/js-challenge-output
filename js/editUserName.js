24d5d4d2dedebbfbf2975f6d51070634b0975457
hsh-newbi <hsonghart0426@gmail.com>
2021-04-04 18:36:00 +0900

    백로그 목록 삭제 기능 추가

--- js/editUserName.js
+++ js/editUserName.js
@@ -1,6 +1,11 @@
 // 1. USER_LS 값이 있을 때만 작동
+<<<<<<< Updated upstream
 // 2. input.value가 있을 때만 사용자 이름 저장
 // 3. input.value 없이 수정하기 버튼을 누르거나 취소 버튼을 누를 경우 팝업 닫기
+=======
+// 2. input.value가 있을 때만 수정
+// 3. input.value가 없이 수정하기 버튼을 누르거나 취소 버튼을 누를 경우 팝업 창 닫기
+>>>>>>> Stashed changes
 
 const editUserNameBox = document.querySelector(".current-username-edit"),
   inputUserName = editUserNameBox.querySelector("input"),
@@ -30,7 +35,7 @@ function printEditPopup(e){
 
   printCurrentName();
   cancelUserName.addEventListener("click", closeEditPopup); // 취소 버튼
-  saveUserName.addEventListener("click", saveValue);
+  saveUserName.addEventListener("click", saveValue); // 저장 버튼
 }
 
 function printCurrentName(){
