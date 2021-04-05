deed7dab1f2c609f7788bb2bb0c906170bc57700
hsh-newbi <hsonghart0426@gmail.com>
2021-04-05 13:19:24 +0900

    Update editUserName.js

--- js/editUserName.js
+++ js/editUserName.js
@@ -1,11 +1,6 @@
 // 1. USER_LS 값이 있을 때만 작동
-<<<<<<< Updated upstream
 // 2. input.value가 있을 때만 사용자 이름 저장
 // 3. input.value 없이 수정하기 버튼을 누르거나 취소 버튼을 누를 경우 팝업 닫기
-=======
-// 2. input.value가 있을 때만 수정
-// 3. input.value가 없이 수정하기 버튼을 누르거나 취소 버튼을 누를 경우 팝업 창 닫기
->>>>>>> Stashed changes
 
 const editUserNameBox = document.querySelector(".current-username-edit"),
   inputUserName = editUserNameBox.querySelector("input"),
