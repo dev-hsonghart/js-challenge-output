84c7434f70a18356f590eb116101b1e7fcd42ece
hsh-newbi <hsonghart0426@gmail.com>
2021-04-06 16:57:43 +0900

    배경색상 랜덤값 조정

--- js/controlBG.js
+++ js/controlBG.js
@@ -1,13 +1,42 @@
 // 1. 새로고침 시 배경이미지가 랜덤으로 뿌려진다.
 
 const bg = document.querySelector(".background-container");
-  
+
+let rgbA= [0, 0, 0],
+  rgbB = [0, 0, 0];
+
+function rgbInit(){
+  rgbA = [0,0,0];
+  rgbB = [0,0,0];
+}
+
+function randomRGB(array){
+  for(let i = 0; i < 2 ; i++){
+    array[i] = getRandomNum();
+  }
+  return array;
+}
+
+function shuffleArray(array){
+  for(let i =0; i< array.length; i++){
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
 function getRandomNum(){
-  return Math.floor(Math.random() * 226);
+  return Math.floor(Math.random() * 256) + 139;
 }
 
 function displayBg(){
-  bg.style.background = `linear-gradient(to right, rgba(${getRandomNum()}, ${getRandomNum()}, ${getRandomNum()}), rgba(${getRandomNum()}, ${getRandomNum()}, ${getRandomNum()}))`
+  rgbInit();
+  randomRGB(rgbA);
+  randomRGB(rgbB);
+  shuffleArray(rgbA);
+  shuffleArray(rgbB);
+
+  bg.style.background = `linear-gradient(to right, rgba(${rgbA[0]}, ${rgbA[1]}, ${rgbA[2]}), rgba(${rgbB[0]}, ${rgbB[1]}, ${rgbB[2]}))`
   bg.style.opacity = 0;
 }
 
